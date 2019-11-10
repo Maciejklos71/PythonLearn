8d990a9a14eb183ca8d3b386b6e3681d23892af6
Maciejklos71 <43652023+Maciejklos71@users.noreply.github.com>
2019-11-10 18:00:30 +0100

    Add files via upload

--- Practice_python/Divisors.py
+++ Practice_python/Divisors.py
@@ -1,6 +1,3 @@
 number = int(input("Give a number to get her divisors"))
-divisor_list =[]
-for i in range(1,number+1):
-    if number%i == 0:
-        divisor_list.append(i)
+divisor_list = [i for i in range(1, number+1) if number % i == 0]
 print(divisor_list)
